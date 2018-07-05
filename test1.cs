d6b44b30b9f260994df6e5d5502c65de100af5ff
warlock40 <hacker26warlock40@gmail.com>
2018-07-05 14:18:27 +0530

    this is change
    hi this is a change

---
+++ test1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefaultNamespace
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+			int a=4;
+			int b = 5;
+			int c = a + b;
+			Console.WriteLine("the answer is {0}", c);
+		}
+	}
+}
